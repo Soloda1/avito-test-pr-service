c21134b2d132b046bf626c7d8fd632c268bb0c6e
Soloda1 <enderderder@gmail.com>
2025-11-13 21:55:24 +0400

    feat: добавил автогенерацию моков для интерфейса Querier в репозитории

--- internal/infrastructure/persistence/postgres/db.go
+++ internal/infrastructure/persistence/postgres/db.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
+//go:generate mockery --name Querier --dir . --output ../../../../mocks --outpkg mocks --with-expecter --filename Querier.go
 type Querier interface {
 	Exec(ctx context.Context, sql string, arguments ...interface{}) (commandTag pgconn.CommandTag, err error)
 	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
