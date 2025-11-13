8d8914f32fdb211a2d72b3471225a22903bbb0ee
Soloda1 <enderderder@gmail.com>
2025-11-13 16:27:52 +0400

    feat: добавил миграции для создания таблиц пользователей, команд и PR

---
+++ migrations/000002_init_indexes.up.sql
@@ -0,0 +1,4 @@
+CREATE INDEX idx_team_members_user_id ON team_members(user_id);
+CREATE INDEX idx_prs_author_id ON prs(author_id);
+CREATE INDEX idx_pr_reviewers_reviewer_id_pr_id ON pr_reviewers(reviewer_id, pr_id);
+CREATE INDEX idx_pr_reviewers_pr_id ON pr_reviewers(pr_id);
