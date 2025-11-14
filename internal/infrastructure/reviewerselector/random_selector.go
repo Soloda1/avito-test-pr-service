25214c60ffd37d25c6c10d666a72759cec611965
Soloda1 <enderderder@gmail.com>
2025-11-14 17:35:17 +0400

    feat: добавил стратегию случайного выбора рецензентов с возможностью настройки генератора случайных чисел

--- internal/infrastructure/reviewerselector/random_selector.go
+++ internal/infrastructure/reviewerselector/random_selector.go
@@ -1,3 +1,46 @@
 package reviewerselector
 
-// TODO: implement random reviewer selection strategy
+import (
+	"math/rand"
+	"time"
+
+	"github.com/google/uuid"
+
+	"avito-test-pr-service/internal/domain/services"
+)
+
+var _ services.ReviewerSelector = (*RandomReviewerSelector)(nil)
+
+type RandomReviewerSelector struct {
+	rnd *rand.Rand
+}
+
+func NewRandomReviewerSelector() services.ReviewerSelector {
+	return NewRandomReviewerSelectorWithRand(rand.New(rand.NewSource(time.Now().UnixNano())))
+}
+
+func NewRandomReviewerSelectorWithRand(r *rand.Rand) services.ReviewerSelector {
+	if r == nil {
+		r = rand.New(rand.NewSource(time.Now().UnixNano()))
+	}
+	return &RandomReviewerSelector{rnd: r}
+}
+
+func (s *RandomReviewerSelector) Select(candidates []uuid.UUID, count int) []uuid.UUID {
+	if count <= 0 || len(candidates) == 0 {
+		return nil
+	}
+
+	shuffled := append([]uuid.UUID(nil), candidates...)
+	if len(shuffled) > 1 {
+		s.rnd.Shuffle(len(shuffled), func(i, j int) {
+			shuffled[i], shuffled[j] = shuffled[j], shuffled[i]
+		})
+	}
+
+	if count > len(shuffled) {
+		count = len(shuffled)
+	}
+
+	return append([]uuid.UUID(nil), shuffled[:count]...)
+}
