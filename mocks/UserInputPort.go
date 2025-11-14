ebdce8e4fa69864cc23f0746b8c09dc408af3b50
Soloda1 <enderderder@gmail.com>
2025-11-14 22:49:17 +0400

    feat: добавил мок-функцию для получения названия команды пользователя

--- mocks/UserInputPort.go
+++ mocks/UserInputPort.go
@@ -144,6 +144,63 @@ func (_c *UserInputPort_GetUser_Call) RunAndReturn(run func(context.Context, uui
 	return _c
 }
 
+// GetUserTeamName provides a mock function with given fields: ctx, id
+func (_m *UserInputPort) GetUserTeamName(ctx context.Context, id uuid.UUID) (string, error) {
+	ret := _m.Called(ctx, id)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetUserTeamName")
+	}
+
+	var r0 string
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, uuid.UUID) (string, error)); ok {
+		return rf(ctx, id)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, uuid.UUID) string); ok {
+		r0 = rf(ctx, id)
+	} else {
+		r0 = ret.Get(0).(string)
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, uuid.UUID) error); ok {
+		r1 = rf(ctx, id)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// UserInputPort_GetUserTeamName_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetUserTeamName'
+type UserInputPort_GetUserTeamName_Call struct {
+	*mock.Call
+}
+
+// GetUserTeamName is a helper method to define mock.On call
+//   - ctx context.Context
+//   - id uuid.UUID
+func (_e *UserInputPort_Expecter) GetUserTeamName(ctx interface{}, id interface{}) *UserInputPort_GetUserTeamName_Call {
+	return &UserInputPort_GetUserTeamName_Call{Call: _e.mock.On("GetUserTeamName", ctx, id)}
+}
+
+func (_c *UserInputPort_GetUserTeamName_Call) Run(run func(ctx context.Context, id uuid.UUID)) *UserInputPort_GetUserTeamName_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context), args[1].(uuid.UUID))
+	})
+	return _c
+}
+
+func (_c *UserInputPort_GetUserTeamName_Call) Return(_a0 string, _a1 error) *UserInputPort_GetUserTeamName_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *UserInputPort_GetUserTeamName_Call) RunAndReturn(run func(context.Context, uuid.UUID) (string, error)) *UserInputPort_GetUserTeamName_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // ListUsers provides a mock function with given fields: ctx
 func (_m *UserInputPort) ListUsers(ctx context.Context) ([]*models.User, error) {
 	ret := _m.Called(ctx)
