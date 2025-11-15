491d52506a1e5601e80a6892789fc9d035eed01f
Soloda1 <enderderder@gmail.com>
2025-11-15 22:42:40 +0400

    refactor: улучшил запрос для получения PRs по рецензенту, оптимизировав соединение с таблицей рецензентов

--- internal/infrastructure/persistence/postgres/pr/repository.go
+++ internal/infrastructure/persistence/postgres/pr/repository.go
@@ -268,12 +268,12 @@ func (r *PRRepository) UpdateStatus(ctx context.Context, prID string, status mod
 func (r *PRRepository) ListPRsByReviewer(ctx context.Context, reviewerID string, status *models.PRStatus) ([]*models.PullRequest, error) {
 	const q = `
 		SELECT p.id, p.title, p.author_id, p.status, p.created_at, p.merged_at, p.updated_at,
-		       COALESCE(array_agg(r2.reviewer_id ORDER BY r2.assigned_at) FILTER (WHERE r2.reviewer_id IS NOT NULL), '{}') AS reviewers
+		       COALESCE(array_agg(r.reviewer_id ORDER BY r.assigned_at) FILTER (WHERE r.reviewer_id IS NOT NULL), '{}') AS reviewers
 		FROM prs p
-		JOIN pr_reviewers r ON p.id = r.pr_id
-		LEFT JOIN pr_reviewers r2 ON p.id = r2.pr_id
-		WHERE r.reviewer_id = @reviewer_id AND (@status IS NULL OR p.status = @status)
+		LEFT JOIN pr_reviewers r ON p.id = r.pr_id
+		WHERE (@status IS NULL OR p.status = @status)
 		GROUP BY p.id, p.title, p.author_id, p.status, p.created_at, p.merged_at, p.updated_at
+		HAVING @reviewer_id = ANY(array_agg(r.reviewer_id))
 		ORDER BY p.created_at DESC;
 	`
 	rows, err := r.querier.Query(ctx, q, pgx.NamedArgs{"reviewer_id": reviewerID, "status": status})
